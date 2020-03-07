1105aff2addeeb9d4049f8bc9bb8aabd6cd3c790
Valeriy Kovalevskiy <vkovalevskiy@wakeapp.ru>
2020-03-07 22:24:52 +0300

    update game logics, added alert

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +34,36 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         firstFlagButton.setImage(UIImage(named: countries[0]), for: .normal)
         secondFlagButton.setImage(UIImage(named: countries[1]), for: .normal)
         thirdFlagButton.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].capitalized
     }
-
+    
+    //MARK: - Actions
+    
+    @IBAction func flagButtonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            score += 1
+            title = "Correct!"
+        } else {
+            title = "Wrong!"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
 }
 
